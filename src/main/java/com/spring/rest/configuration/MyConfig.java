73f6d2acb396357c04321efae8599832ef8fe9c7
Andrey <x137@yandex.ru>
2021-12-27 22:01:25 +0300

    Add dependency, add MyConfig

---
+++ src/main/java/com/spring/rest/configuration/MyConfig.java
@@ -0,0 +1,10 @@
+package com.spring.rest.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("main.java.com.spring.rest.configuration")
+public class MyConfig {
+
+}
